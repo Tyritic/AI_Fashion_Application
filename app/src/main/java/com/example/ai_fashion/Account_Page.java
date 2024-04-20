cc83f71bce814d08d86661f8fbb4798089873f99
Tyritic <1553206659@qq.com>
2024-04-20 20:38:27 +0800

    实现首页导航栏

---
+++ app/src/main/java/com/example/ai_fashion/Account_Page.java
@@ -0,0 +1,19 @@
+package com.example.ai_fashion;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Insert;
+
+public class Account_Page extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_account_page);
+    }
+}
--- app/src/main/java/com/example/ai_fashion/Mine_Fragment.java
+++ app/src/main/java/com/example/ai_fashion/Mine_Fragment.java
@@ -1,9 +1,12 @@
 package com.example.ai_fashion;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
 
@@ -14,6 +17,17 @@ public class Mine_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mine, container, false);
+        View view = inflater.inflate(R.layout.fragment_mine, container, false);
+        // Find the button and set the click listener
+        Button accountInformationButton = view.findViewById(R.id.account_information_button);
+            accountInformationButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Create an Intent to start Account_Page
+                    Intent intent = new Intent(getActivity(), Account_Page.class);
+                    startActivity(intent);
+                }
+            });
+            return view;
     }
 }
